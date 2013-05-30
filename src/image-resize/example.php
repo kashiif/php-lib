71a4c5f0e14f27dbd16fd98e4abcf8849cb2f4ad
unknown <kkhan@PAK-KKHAN.workforce.wfs>
2013-05-30 17:19:32 +0500

    Added image resize class

---
+++ src/image-resize/example.php
@@ -0,0 +1,35 @@
+<?php
+
+// Resize With Exact Size - Image may lose aspect ratio
+
+$resize = new ResizeImage('images/Be-Original.jpg');
+$resize->resizeTo(100, 100, 'exact');
+$resize->saveImage('images/be-original-exact.jpg');
+
+// Resize Max Width Size
+
+$resize = new ResizeImage('images/Be-Original.jpg');
+$resize->resizeTo(100, 100, 'maxWidth');
+$resize->saveImage('images/be-original-maxWidth.jpg');
+
+// Resize Max Height Size
+
+$resize = new ResizeImage('images/Be-Original.jpg');
+$resize->resizeTo(100, 100, 'maxHeight');
+$resize->saveImage('images/be-original-maxHeight.jpg');
+
+// Resize Auto Size From Given Width And Height
+
+$resize = new ResizeImage('images/Be-Original.jpg');
+$resize->resizeTo(100, 100);
+$resize->saveImage('images/be-original-default.jpg');
+
+// Download The Resized Image
+
+$resize = new ResizeImage('images/Be-Original.jpg');
+$resize->resizeTo(100, 100, 'exact');
+$resize->saveImage('images/be-original-exact.jpg', "100", true);
+
+
+?>
+
